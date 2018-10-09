a88a770db330ff95c6d489300d3e8d51a0f52315
Alfredo Mazzinghi <am2419@cl.cam.ac.uk>
2018-10-09 14:53:33 +0100

    Use CheriABI cpuset syscalls implementation in the default kernel.

--- sys/kern/kern_cpuset.c
+++ sys/kern/kern_cpuset.c
@@ -1505,7 +1505,7 @@ cpuset_setproc_update_set(struct proc *p, struct cpuset *set)
 
 #ifndef _SYS_SYSPROTO_H_
 struct cpuset_args {
-	cpusetid_t	*setid;
+	cpusetid_t	* __capability setid;
 };
 #endif
 int
@@ -1575,7 +1575,7 @@ struct cpuset_getid_args {
 	cpulevel_t	level;
 	cpuwhich_t	which;
 	id_t		id;
-	cpusetid_t	*setid;
+	cpusetid_t * __capability setid;
 };
 #endif
 int
@@ -1642,7 +1642,7 @@ struct cpuset_getaffinity_args {
 	cpuwhich_t	which;
 	id_t		id;
 	size_t		cpusetsize;
-	cpuset_t	*mask;
+	cpuset_t * __capability mask;
 };
 #endif
 int
@@ -1760,7 +1760,7 @@ struct cpuset_setaffinity_args {
 	cpuwhich_t	which;
 	id_t		id;
 	size_t		cpusetsize;
-	const cpuset_t	*mask;
+	const cpuset_t * __capability mask;
 };
 #endif
 int
@@ -1887,8 +1887,8 @@ struct cpuset_getdomain_args {
 	cpuwhich_t	which;
 	id_t		id;
 	size_t		domainsetsize;
-	domainset_t	*mask;
-	int 		*policy;
+	domainset_t * __capability mask;
+	int * __capability policy;
 };
 #endif
 int
@@ -2023,7 +2023,7 @@ struct cpuset_setdomain_args {
 	cpuwhich_t	which;
 	id_t		id;
 	size_t		domainsetsize;
-	domainset_t	*mask;
+	domainset_t * __capability mask;
 	int 		policy;
 };
 #endif
